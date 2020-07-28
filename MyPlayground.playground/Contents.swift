f092d8d3c1067998bdaaeadc0479c0a81127fbcd
ryuzo hiruma <>
2020-07-28 22:47:54 +0900

    swift1.1.1新規作成

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+/*bool型、String型、Int型、Double型の変数を定義し、
+コンソールにログを出力する。(Print)*/
+
+//Bool
+let bool : Bool = true
+print(bool)
+
+//String
+let hello : String = "こんにちは"
+print(hello)
+
+//int
+let one : Int = 1
+let two = 2
+print(one+two)
+
+//double
+let doubleNumber : Double = 1.1
+print(doubleNumber)
+
+let aaa = Double(one) + doubleNumber
+print (aaa)
