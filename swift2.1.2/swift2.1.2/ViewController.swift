f4ae3350ba9ddcc8df6923f56d8e0cc12bea25c9
ryuzo hiruma <>
2020-07-31 05:11:48 +0900

    swift2.1.2新規作成

---
+++ swift2.1.2/swift2.1.2/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  swift2.1.2
+//
+//  Created by 比留間龍三 on 2020/07/31.
+//  Copyright © 2020 すいふと. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+}
+}
