71acf219da78ef8b0240344f79674df431da03f0
ryuzo hiruma <>
2020-07-31 04:34:35 +0900

    swift2.1.1新規作成

---
+++ swift2.1.1/swift2.1.1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  swift2.1.1
+//
+//  Created by 比留間龍三 on 2020/07/31.
+//  Copyright © 2020 すいふと. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let swift = NSLocalizedString("I started the Swift course.", comment: "")
+        print(swift)
+    }
+
+
+}
+
