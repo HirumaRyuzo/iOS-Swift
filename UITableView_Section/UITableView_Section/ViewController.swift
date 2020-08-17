23361994d69a616e4b4666a83d1743042af23ca0
ryuzo hiruma <>
2020-08-18 01:15:58 +0900

    セルの可変できんがな！

--- UITableView_Section/UITableView_Section/ViewController.swift
+++ UITableView_Section/UITableView_Section/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController ,UITableViewDelegate, UITableViewDataSource{
+struct HogeItem: Decodable {
+    let Name: String
+    let Image: String
+}
+
+struct HogeSection: Decodable {
+    let SectionName: String
+    let Items: [HogeItem]
+}
+
+var _items: [HogeSection] = []
+
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
 // MARK: - pattern①Plistなし
 //    //ディズニービッグ5のメンツを配列に設定
@@ -36,61 +48,63 @@ class ViewController: UIViewController ,UITableViewDelegate, UITableViewDataSour
 //        super.viewDidLoad()
 //        // Do any additional setup after loading the view.
 //    }
-    
-    
-// MARK: - pattern②Plistあり
-     var _items:NSArray = []
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-
-            // Plistファイルパス
-            let path = Bundle.main.path(forResource: "DisneyCharacterList", ofType:"plist")
-            _items = NSArray(contentsOfFile:path!)!
-            //println(_items);
+    // MARK: - pattern②Plistあり
+    @IBOutlet weak var commentTableView: UITableView!
+    @IBOutlet weak var image: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let path = Bundle.main.path(forResource: "DisneyCharacterList", ofType:"plist" ) {
+            let data = try! Data(contentsOf: URL(fileURLWithPath: path))
+            _items = try! PropertyListDecoder().decode([HogeSection].self, from: data)
+        }
+        
+        //高さを可変にする
+        commentTableView.estimatedRowHeight = 66
+        commentTableView.rowHeight = UITableView.automaticDimension
     }
     
     // 設定
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return _items.count
     }
     
     // 設定（行数）
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
-        -> Int {
-            return _items.count
-    }
-    
-    // 設定（セル）
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell = tableView.dequeueReusableCell(withIdentifier: "DisneyCell", for: indexPath as IndexPath)
-        let dic = _items.object(at: indexPath.row) as! NSDictionary
-        cell.textLabel!.text = dic.value(forKey: "Name") as? String
-        let imageName = dic.value(forKey: "Image") as! String
-        cell.imageView?.image = UIImage(named: imageName)
-        return cell
-    }
-    //セルタップ時にセルの内容を取得する
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "DisneyCell", for: indexPath as IndexPath)
-            let dic = _items.object(at: indexPath.row) as! NSDictionary
-            cell.textLabel!.text = dic.value(forKey: "Name") as? String
-            print(cell.textLabel!.text as Any)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return _items[section].Items.count
     }
     
     //セクション数の設定
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return _items.count
     }
     
-    //セクション名設定
-    let sectionTitles = ["セクション1","セクション2"]
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?
-    {
-
-        return sectionTitles[section]
+    // 設定（セクションタイトル）
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return _items[section].SectionName
     }
-
+    
+    // 設定（セル）
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = _items[indexPath.section].Items[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DisneyCell", for: indexPath as IndexPath)
+        cell.textLabel?.numberOfLines=0
+        cell.textLabel?.text = item.Name
+        cell.imageView?.image = UIImage(named: item.Image)
+        
+        //セルのサイズを指定
+//        image.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        return cell
+    }
+    
+    //セルタップ時にセルの内容を取得する
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = _items[indexPath.section].Items[indexPath.row]
+        
+        print(item.Name)
+    }
 }
 
