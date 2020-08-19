5188c9ba8e2a1c5ee9cf31864e4bffc0d5794ec5
ryuzo hiruma <>
2020-08-19 23:45:06 +0900

    画像の追加やらなんやら

---
+++ CollectionView_NoSection/CollectionView_NoSection/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  CollectionView_NoSection
+//
+//  Created by 比留間龍三 on 2020/08/16.
+//  Copyright © 2020 すいふと. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
--- CollectionView_NoSection/CollectionView_NoSection/ViewController.swift
+++ CollectionView_NoSection/CollectionView_NoSection/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SceneDelegate.swift
 //  CollectionView_NoSection
 //
 //  Created by 比留間龍三 on 2020/08/16.
@@ -7,62 +7,57 @@
 //
 
 import UIKit
- 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
-    private let photos = ["0.MickeyIcon", "1.MinnieIcon", "2.DonaldIcon", "3.GoofyIcon", "4.PlutoIcon"]
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView:UICollectionView!
+    
+    // 画像
+    let photos = ["0.MickeyIcon", "0.MickeyIcon","0.MickeyIcon","1.MinnieIcon","1.MinnieIcon",
+                  "1.MinnieIcon","2.DonaldIcon","3.GoofyIcon","img8"]
+    
+    // レイアウト設定
+    let sectionInsets = UIEdgeInsets(top: 10.0, left: 2.0, bottom: 2.0, right: 2.0)
+    // 1行あたりのアイテム数
+    let itemsPerRow: CGFloat = 3
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-    // １つのセクションの中に表示するセル（要素）の数。
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return photos.count
     }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
 
-    //セル（要素）に表示する内容
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // "Cell" の部分は　Storyboard でつけた cell の identifier。
-        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        guard let cell:CollectionViewCell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",
+                                               for: indexPath) as? CollectionViewCell else {
+                                                fatalError("Wrong cell class dequeued")}
 
-        // Tag番号を使ってインスタンスをつくる
-        let photoImageView = cell.contentView.viewWithTag(1)  as! UIImageView
-        let photoImage = UIImage(named: photos[indexPath.row])
-        photoImageView.image = photoImage
+        let image = UIImage(named: photos[indexPath.row])
+
+        cell.iconImageView.image = image
+        cell.label.text = photos[indexPath.row]
         
         return cell
     }
-    
-    // レイアウト設定　UIEdgeInsets については下記の参考図を参照。
-    private let sectionInsets = UIEdgeInsets(top: 10.0, left: 2.0, bottom: 2.0, right: 2.0)
-    // 1行あたりのアイテム数
-    private let itemsPerRow: CGFloat = 2
+
     
     // Screenサイズに応じたセルサイズを返す
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//
-//        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
-//        let availableWidth = view.frame.width - paddingSpace
-//        let widthPerItem = availableWidth / itemsPerRow
-//        return CGSize(width: widthPerItem, height: widthPerItem + 42)
-//        
-//    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        let width: CGFloat = view.frame.width / 3 - 2
-        let height: CGFloat = width
-        return CGSize(width: width, height: height)
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = collectionView.frame.width - paddingSpace
+        let widthPerItem = availableWidth / 3
+        return CGSize(width: widthPerItem, height: collectionView.frame.height / 3)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return sectionInsets
-    }
-
-    // セルの行間の設定
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10.0
     }
     
 }
