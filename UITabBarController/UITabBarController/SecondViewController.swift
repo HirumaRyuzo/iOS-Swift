cfa79b209b065517a4d6d9a445ed860a401e7cb8
ryuzo hiruma <>
2020-08-21 09:30:06 +0900

    UITabBarController　新規追加

---
+++ UITabBarController/UITabBarController/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  UITabBarController
+//
+//  Created by 比留間龍三 on 2020/08/20.
+//  Copyright © 2020 すいふと. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UITabBarController/UITabBarController/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  UITabBarController
+//
+//  Created by 比留間龍三 on 2020/08/20.
+//  Copyright © 2020 すいふと. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
