5b15173dc19123f19ddbd07fbb32318246159045
ryuzo hiruma <>
2020-08-27 01:08:05 +0900

    instagram 新規作成

---
+++ instagram/instagram/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by 比留間龍三 on 2020/08/26.
+//  Copyright © 2020 すいふと. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class ViewController: UIViewController {
+var controller: UIDocumentInteractionController! = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func share(_ sender: Any) {
+        do {
+
+           let Username =  "ryusan.e.disney" // Your Instagram Username here
+            let appURL = URL(string: "instagram://user?username=\(Username)")!
+            let application = UIApplication.shared
+
+            if application.canOpenURL(appURL) {
+                application.open(appURL)
+            } else {
+                // if Instagram app is not installed, open URL inside Safari
+                let webURL = URL(string: "https://instagram.com/\(Username)")!
+                application.open(webURL)
+            }
+    
+}
+}}
