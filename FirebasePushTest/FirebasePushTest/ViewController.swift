fb8a7fe831d1ad05cff0de74f2512c4d717e7029
ryuzo hiruma <>
2020-09-04 12:36:48 +0900

    PHP PUSH 新規作成

---
+++ FirebasePushTest/FirebasePushTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebasePushTest
+//
+//  Created by 比留間龍三 on 2020/08/30.
+//  Copyright © 2020 すいふと. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FirebasePushTest/Pods/Target Support Files/Pods-FirebasePushTest/Pods-FirebasePushTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirebasePushTest : NSObject
+@end
+@implementation PodsDummy_Pods_FirebasePushTest
+@end
