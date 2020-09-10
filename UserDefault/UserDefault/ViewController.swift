7d54f65a4d39037ca501ec974ab7e6fd8f137af3
ryuzo hiruma <>
2020-09-10 17:54:17 +0900

    userDefaults.synchronize()を追加

--- UserDefault/UserDefault/ViewController.swift
+++ UserDefault/UserDefault/ViewController.swift
@@ -13,50 +13,61 @@ class ViewController: UIViewController, UITextFieldDelegate  {
     @IBOutlet var textField:UITextField!
     @IBOutlet var label:UILabel!
     
-    //ラベルの初期値
-    var testText:String = "初期値"
-    let userDefaults = UserDefaults.standard
+       var testText:String = "default"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        textField.delegate = self
-        userDefaults.register(defaults: ["DataStore": "初期値"])
-        label.text = readData()
-    }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
-        guard let text = textField.text else { return false }
-        testText = text
-        label.text = testText
+       // UserDefaults のインスタンス
+       let userDefaults = UserDefaults.standard
     
-        // キーボードを閉じる
-        textField.resignFirstResponder()
-        saveData(str: testText)
-        
-        return true
-    }
+       override func viewDidLoad() {
+           super.viewDidLoad()
     
-    func saveData(str: String){
-        // Keyを指定して保存
-        userDefaults.set(str, forKey: "DataStore")
-    }
+           // textFiel の情報を受け取るための delegate を設定
+           textField.delegate = self
+    
+           // デフォルト値
+           userDefaults.register(defaults: ["DataStore": "default"])
+    
+           label.text = readData()
+    
+       }
+    
+       func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+    
+           testText = textField.text!
+    
+           label.text = testText
+    
+           // キーボードを閉じる
+           textField.resignFirstResponder()
+    
+           saveData(str: testText)
+            userDefaults.synchronize()
+           return true
+       }
+    
+       func saveData(str: String){
+    
+           // Keyを指定して保存
+        UserDefaults.standard.set(str, forKey: "DataStore")
 
-    func readData() -> String {
-        // Keyを指定して読み込み
-        guard let str: String = userDefaults.object(forKey: "DataStore") as? String else { return "error" }
-        
-            return str
-    }
+       }
     
+       func readData() -> String {
+           // Keyを指定して読み込み
+           let str: String = userDefaults.object(forKey: "DataStore") as! String
+    
+           return str
+       }
     
     @IBAction func deleteButtonTapped(_ sender : AnyObject) {
-        // Key の値を削除
-        userDefaults.removeObject(forKey: "DataStore")
+         // Key の値を削除
+               userDefaults.removeObject(forKey: "DataStore")
         
-        // Keyを指定して読み込み
-        guard let str: String = userDefaults.object(forKey: "DataStore") as? String else { return }
-        label.text = str
-    }
+               // Keyを指定して読み込み
+               let str: String = userDefaults.object(forKey: "DataStore") as! String
+        
+               label.text = str
+           }
 }
 
